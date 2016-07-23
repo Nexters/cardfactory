b848b224b58bf55d0835c88ffd585f483b5ceee7
Donghyun Go <godong9@gmail.com>
2016-07-23 19:54:44 +0900

    Update

---
+++ cardfactory/db/db.js
@@ -0,0 +1,15 @@
+var mysql = require('mysql');
+var config = require('../config/index');
+var pool = mysql.createPool({
+  connectionLimit : 10,
+  host            : config.db.host,
+  port            : config.db.port,
+  user            : config.db.user,
+  password        : config.db.password,
+  database: 'myfolio'
+});
+
+module.exports = {
+  mysql: mysql,
+  pool: pool
+};
