6e8cdc76af35dab172fb99bb61910e09af1c220f
Donghyun Go <godong9@gmail.com>
2016-07-26 15:42:15 +0900

    Add users page

--- cardfactory/routes/users.js
+++ cardfactory/routes/users.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* GET user page */
+router.get('/:id', function(req, res, next) {
+  res.render('user-page', { title: '유저' });
 });
 
 router.post('/login', function(req, res, next) {
