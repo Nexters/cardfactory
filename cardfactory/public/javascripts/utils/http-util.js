99b9247e44b5448a7dd31abeb3cbb5fb3dca84c3
Donghyun Go <godong9@gmail.com>
2016-07-26 22:24:48 +0900

    Update http-util

---
+++ cardfactory/public/javascripts/utils/http-util.js
@@ -0,0 +1,79 @@
+//card-controller.js
+define([
+  '../bower_components/jquery/dist/jquery'
+], function (
+  $
+) {
+  'use strict';
+
+  function HttpUtil() {
+
+  }
+
+  HttpUtil.getData = function(url, params, callback) {
+    $.ajax({
+      url: url,
+      type: 'GET',
+      data: params,
+      error: function errorHandler(jqXHR, textStatus, errorThrown) {
+        callback("Error");
+      },
+      success: function successHandler(data, status, xhr) {
+        callback(null, data)
+      }
+    });
+  };
+
+  HttpUtil.postData = function(url, params, callback) {
+    $.ajax({
+      url: url,
+      type: 'POST',
+      contentType: 'application/json;charset=UTF-8',
+      dataType: 'json',
+      data: JSON.stringify(params),
+      error: function errorHandler(jqXHR, textStatus, errorThrown) {
+        callback("Error");
+      },
+      success: function successHandler(data, status, xhr) {
+        callback(null, data)
+      }
+    });
+  };
+
+  HttpUtil.putData = function(url, params, callback) {
+    $.ajax({
+      url: url,
+      type: 'PUT',
+      contentType: 'application/json;charset=UTF-8',
+      dataType: 'json',
+      data: JSON.stringify(params),
+      error: function errorHandler(jqXHR, textStatus, errorThrown) {
+        callback("Error");
+      },
+      success: function successHandler(data, status, xhr) {
+        callback(null, data)
+      }
+    });
+  };
+
+  HttpUtil.deleteData = function(url, params, callback) {
+    $.ajax({
+      url: url,
+      type: 'DELETE',
+      contentType: 'application/json;charset=UTF-8',
+      dataType: 'json',
+      data: JSON.stringify(params),
+      error: function errorHandler(jqXHR, textStatus, errorThrown) {
+        callback("Error");
+      },
+      success: function successHandler(data, status, xhr) {
+        callback(null, data)
+      }
+    });
+  };
+
+  return HttpUtil;
+});
+
+
+
