fea54b3e7404f94cfc0e391b5720060483d7557d
Donghyun Go <godong9@gmail.com>
2016-07-27 16:29:01 +0900

    Add apidoc to images router

--- cardfactory/routes/images.js
+++ cardfactory/routes/images.js
@@ -2,8 +2,20 @@ var express = require('express');
 var router = express.Router();
 var ImageController = require('../controllers/image');
 
+/**
+ * @api {get} /images/:name Get Image
+ * @apiName GetImage
+ * @apiGroup Image
+ *
+ * @apiParam {String} name Image name
+ */
 router.get('/:name', ImageController.getImage);
 
+/**
+ * @api {get} /images/upload Post Image
+ * @apiName PostImage
+ * @apiGroup Image
+ */
 router.post('/upload', ImageController.checkFileSize, ImageController.upload);
 
 module.exports = router;
