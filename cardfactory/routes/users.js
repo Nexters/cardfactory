df067e4918cef3690850bfe995c0650ef5fdcd87
festinalente <nasuk0610@gmail.com>
2016-07-27 14:01:38 +0900

    Update user apidoc

--- cardfactory/routes/users.js
+++ cardfactory/routes/users.js
@@ -2,11 +2,34 @@ var express = require('express');
 var router = express.Router();
 var UserController = require('../controllers/user');
 
-/* GET user page */
+/**
+ * @api {get} /users Get user page
+ * @apiName GetUserPage
+ * @apiGroup User
+ *
+ * @apiParam {String} id User unique id
+ */
 router.get('/:id', UserController.getUserPageById);
 
+/**
+ * @api {post} /users Login
+ * @apiName PostLogin
+ * @apiGroup User
+ *
+ * @apiParam {String} email User email
+ * @apiParam {String} password User User password
+ */
 router.post('/login', UserController.postLogin);
 
+/**
+ * @api {post} /users join
+ * @apiName UserJoin
+ * @apiGroup User
+ *
+ * @apiParam {String} nickname User nickname
+ * @apiParam {String} email User email
+ * @apiParam {String} password User User password
+ */
 router.post('/join', UserController.postJoin);
 
 module.exports = router;
