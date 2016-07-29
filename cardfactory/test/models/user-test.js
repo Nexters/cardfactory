a8ebf8f045c69638609bfb989f158e76ed3d458c
festinalente <nasuk0610@gmail.com>
2016-07-29 16:36:51 +0900

    Update user test

--- cardfactory/test/models/user-test.js
+++ cardfactory/test/models/user-test.js
@@ -25,4 +25,22 @@ describe('User', function() {
       });
     });
   });
+  describe('#getByEmail', function(){
+    it('should get user data by email', function(done){
+      User.getByEmail({'email': 'test@test.com'}, function(err, result){
+        expect(err).to.not.exist;
+        expect(result.id).to.equal(1);
+        done();
+      });
+    })
+  });
+  describe('#create', function(){
+    it('should create a user', function(done){
+      User.create({'nickname':'testNickname2','email':'test2@test.com','password':'test2'},function(err,result){
+        expect(err).to.not.exist;
+        expect(result).to.not.be.null;
+        done();
+      });
+    })
+  });
 });
