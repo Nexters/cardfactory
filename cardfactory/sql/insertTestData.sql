db9b44b816070517fd16e058e559dedc6c87d740
festinalente <nasuk0610@gmail.com>
2016-08-03 14:14:06 +0900

    Update sql
    added card columns

--- cardfactory/sql/insertTestData.sql
+++ cardfactory/sql/insertTestData.sql
@@ -1,3 +1,3 @@
 insert into user (id, nickname, email, password) values (1, 'testNickname', 'test@test.com', 'test');
 insert into card_type (id, name, defaultImg, defaultFont) values (1, 'cardname','defaultImg', 'defaultFont');
-insert into card (id, img, source, font, content, hitsCount, likesCount, userId, cardTypeId) values (1, 'testImg', 'testSource', 'testFont', 'testContent', 1, 1, 1, 1);
+insert into card (id, img, content, source, font, fontSize, brightness, hitsCount, likesCount, userId, cardTypeId) values (1, 'testImg', 'testContent', 'testSource', 'testFont', 10, 1, 1, 1, 1, 1);
