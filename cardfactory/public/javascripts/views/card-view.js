98414827dc2ed126913530c896946e3c26cbb75f
Donghyun Go <godong9@gmail.com>
2016-08-13 23:14:01 +0900

    Update card edit

--- cardfactory/public/javascripts/views/card-view.js
+++ cardfactory/public/javascripts/views/card-view.js
@@ -4,12 +4,13 @@ define([
 ) {
   'use strict';
 
+  var MAX_ROWS = 2;
+
   function CardView(template) {
     this.template = template;
 
     this.$container = $('#card_container');
     this.$imageMenu = $('#image_menu_option');
-    this.$templateMenu = $('#template_menu_option');
     this.$fontMenu = $('#font_menu_option');
     this.$fontSizeMenu = $('#font_size_menu_option');
     this.$saveCardBtn = $('#save_card_btn');
@@ -24,16 +25,26 @@ define([
         self.$content = $('#card_content');
         self.$source = $('#card_source');
         self.applyStyle();
+
+        self.$content.unbind('keydown').keydown(function(event) {
+          var rows = $(this).val().split('\n').length;
+          if (rows === 1) {
+            $(this).css('height', '60px');
+          } else if (rows === 2) {
+            $(this).css('height', '120px');
+          }
+
+          // 2줄 넘어가서 엔터 쳤을 때 엔터키 막기 위한 처리
+          if (event.which === 13 && rows >= MAX_ROWS) {
+            return false;
+          }
+        });
       },
       drawMenu: function () {
         switch(params.menu) {
           case 'image':
             self.$imageMenu.show();
             self.$imageMenu.siblings().hide();
-            break;
-          case 'template':
-            self.$templateMenu.show();
-            self.$templateMenu.siblings().hide();
             break;
           case 'font':
             self.$fontMenu.show();
@@ -54,15 +65,21 @@ define([
 
   CardView.prototype.applyStyle = function() {
     var coverImg = this.$cardItem.data('img');
+    var fontSize = this.$cardItem.data('fontsize');
     this.$cardItem.css({
       'background-image': 'url("' + coverImg + '")',
       'background-size': 'cover'
     });
 
+    this.$content.css({
+      'font-size': fontSize + 'px'
+    })
+
   };
 
   CardView.prototype.bind = function(event, handler) {
     var self = this;
+
     if (event === 'saveCard') {
       self.$saveCardBtn.click(function() {
         // TODO: 데이터 가져오기
@@ -103,11 +120,15 @@ define([
     }
 
     if (event === 'changeFont'){
-
+      $('#font_menu_option > img').click(function() {
+        handler($(this).data('font'));
+      });
     }
 
     if (event === 'changeFontSize'){
-
+      $('#font_size_menu_option > img').click(function() {
+        handler($(this).data('fontsize'));
+      });
     }
   };
 
