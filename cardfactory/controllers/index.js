61e98cba7e88d9cc036982dcfd912d23fc8178af
festinalente91 <nasuk0610@gmail.com>
2016-08-14 04:42:50 +0900

    Update login page

--- cardfactory/controllers/index.js
+++ cardfactory/controllers/index.js
@@ -7,6 +7,10 @@ IndexController.getMainPage = function(req, res, next) {
   res.render('index', { title: '메인', name: 'tee' });
 };
 
+IndexController.getLoginPage = function(req, res, next) {
+  res.render('login-page');
+};
+
 IndexController.getJoinPage = function(req, res, next) {
   res.render('join-page', { title: '회원가입' });
 };
