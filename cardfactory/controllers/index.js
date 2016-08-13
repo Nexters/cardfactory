3cbb71949615fd93f4878ced8bf9c3221d4a414c
shincheeun <harusce12@gmail.com>
2016-08-14 04:47:55 +0900

    fix login error(title)

--- cardfactory/controllers/index.js
+++ cardfactory/controllers/index.js
@@ -8,7 +8,7 @@ IndexController.getMainPage = function(req, res, next) {
 };
 
 IndexController.getLoginPage = function(req, res, next) {
-  res.render('login-page');
+  res.render('login-page', {title: '로그인'});
 };
 
 IndexController.getJoinPage = function(req, res, next) {
